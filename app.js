223084f3dfb05d8e9ec0306e38580a6d5a06302a
Irfan <irfanali987654@gmail.com>
2021-02-23 05:33:39 +0530

    completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const taskInput= document.querySelector('#task');
 const form= document.querySelector('#task-form');
 const taskList= document.querySelector('.collection');
 const clearTask= document.querySelector('.clear-tasks');
+const filter=document.querySelector('#filter');
 
 loadEventListener();
 
@@ -9,6 +10,8 @@ function loadEventListener() {
 
     form.addEventListener('submit',addTask);
     clearTask.addEventListener('click',clear);
+    taskList.addEventListener('click',removeTask);
+    filter.addEventListener('keyup',filterTask);
 
 }
 function addTask(e) {
@@ -39,5 +42,30 @@ function addTask(e) {
 function clear (e) {
 
     taskList.innerHTML='';
+    
+}
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')){
+        e.target.parentElement.parentElement.remove();
+    }
+    
+}
+function filterTask(e) {
+    const text=e.target.value.toLowerCase();
+    // console.log(text);
+    document.querySelectorAll('.collection-item').forEach
+    (function (task) {
+
+        const item = task.firstChild.textContent;
+
+        if(item.toLowerCase().indexOf(text)!= -1){
+            task.style.display='block';
+        }
+        else {
+            task.style.display='none';
+        }
+    });
+
+
     
 }
